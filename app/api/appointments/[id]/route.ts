00b5a033cef3417646500056805272953f9b61f5
Oguzhan Kurt <oguzhan.kurt@outlook.at>
2026-05-27 16:50:29 +0200

    fix nextjs delete params

--- app/api/appointments/[id]/route.ts
+++ app/api/appointments/[id]/route.ts
@@ -2,20 +2,26 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function DELETE(
-  req: Request,
-  { params }: {
-    params: {
+  request: Request,
+  context: {
+    params: Promise<{
       id: string;
-    };
+    }>;
   }
 ) {
 
   try {
 
+    const params =
+      await context.params;
+
+    const id =
+      Number(params.id);
+
     await prisma.appointment.delete({
 
       where: {
-        id: Number(params.id),
+        id,
       },
 
     });
